8de887894aca9015932ed8ad3b8690d901642348
Gerwin Sturm <scarygami@gmail.com>
2014-11-07 17:26:45 +0100

    Add Healthcheck

--- bin/server.dart
+++ bin/server.dart
@@ -3,10 +3,28 @@ import 'package:appengine/appengine.dart';
 import 'package:endpoints_tictactoe/tictactoe_api.dart';
 
 import 'dart:io';
+import 'dart:convert' show UTF8;
 
 ApiServer api_server;
 
+_healthResponse(HttpRequest request) {
+  request.drain().then((_) {
+    var response = request.response;
+    var data = UTF8.encode('ok');
+    response.headers.contentType =
+      new ContentType('text', 'plain', charset: 'charset=utf-8');
+    response.statusCode = 200;
+    response.contentLength = data.length;
+    response.add(data);
+    response.close();
+  });
+}
+
 _handler(HttpRequest request) {
+  if (request.uri.path.startsWith('/_ah/health')) {
+    _healthResponse(request);
+    return;
+  }
   if (request.uri.path.startsWith('/_ah/spi/')) {
     api_server.handleRequest(request);
     return;
